a4d5b3254619694808a3f7ff749b6116d2409389
Denis-Paul <paul_denis100@yahoo.com>
2025-04-21 09:33:57 +0300

    Added products

---
+++ spec/views/admin/products/show.html.tailwindcss_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "admin/products/show", type: :view do
+  before(:each) do
+    assign(:admin_product, Admin::Product.create!(
+      name: "Name",
+      description: "MyText",
+      price: 2,
+      category: nil,
+      active: false
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(//)
+    expect(rendered).to match(/false/)
+  end
+end
